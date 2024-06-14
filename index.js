59a13e3a9401cfd6e218a409a4ff9abc9ec4039e
Victor Ordozgoite <victorordozgoite@Victors-Air.local>
2024-06-14 00:50:56 -0400

    Added MQTT

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config();
+const mqtt = require('mqtt');
+
+// URL do broker MQTT com TLS
+const brokerUrl = 'mqtts://ecd8950746cc4cbe99f19f8a4d3a2f23.s1.eu.hivemq.cloud:8883';
+const options = {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+};
+
+// Conectar ao broker
+const client = mqtt.connect(brokerUrl, options);
+
+client.on('connect', () => {
+    console.log('Connected to MQTT broker');
+
+    // Inscrever-se em um tópico específico
+    client.subscribe('dse/register', (err) => {
+        if (!err) {
+            console.log('Subscribed to topic');
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    const msg = message.toString();
+
+    console.log(`Received message: ${msg} on topic: ${topic}`);
+
+    // if (msg === 'mensagem_especifica') {
+    //     suaFuncao();
+    // }
+});
+
+// function suaFuncao() {
+//     console.log('Função executada em resposta à mensagem específica');
+// }
 
 const recognitionAttemptRoute = require('./routes/RecognitionAttempt');
 
